22e6a03375b61ae8cef1f43bd6e8d21e87ccd8f8
hamabu0531 <hamabu0531@icloud.com>
2024-08-28 15:56:18 +0900

    アイコンの追加

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowIcon(QIcon("C:\\Users\\hamab\\Downloads\\memo.png")); // アプリアイコンの設定
     w.show();
     return a.exec();
 }
